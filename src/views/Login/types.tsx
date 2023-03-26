ff10595bbb4e14b84f393466f54a2a6e13f6cb03
qiuye <1464815392@qq.com>
2023-03-26 10:12:53 +0800

    feat: [登录页面] 1.登录页面表单校验规则添加

--- src/views/Login/types.tsx
+++ src/views/Login/types.tsx
@@ -1,11 +1,40 @@
-import React, {useMemo} from "react";
+import React, {useCallback, useMemo, useState} from "react";
 import { UserOutline,MailOutline } from "antd-mobile-icons";
-export type Icons = 'phone' | 'code'
-export type UseLogin = () => {
-    icons:Record<Icons, JSX.Element>,
-}
-export const useLogin:UseLogin = () => {
+import {Rule} from "rc-field-form/lib/interface";
 
+export type Icons = 'phone' | 'code'
+export type OnSubmit = () => void;
+export type GetCode = (e:React.MouseEvent) => void;
+export type UseLogin = (formInstance:any) => {
+    icons:Record<Icons, JSX.Element>,
+    rules:Record<string, Rule[]>,
+    onSubmit:OnSubmit,
+    getCode:GetCode,
+}
+export const useLogin:UseLogin = (formInstance:any) => {
+    /* 校验规则 */
+    const [rules] = useState<Record<string, Rule[]>>({
+        phone:[
+            {validator:(_,value) => {
+                value = value?.trim();
+                return new Promise((resolve, reject) => {
+                    if(!value) return reject('请输入手机号');
+                    const reg = /^(?:(?:\+|00)86)?1[3-9]\d{9}$/;
+                    if(reg.test(value)) return resolve('');
+                    else return reject('输入的手机号不合法')
+                })
+            }}
+        ],
+        code:[
+            {validator:(_,value) => {
+                value = value?.trim();
+                return new Promise((resolve, reject) => {
+                    if(!value) return reject('请输入验证码');
+                    else resolve('');
+                })
+            }}
+        ],
+    })
     const icons = useMemo<Record<string, JSX.Element>>(() => {
         return {
             phone:<UserOutline className={'zhuhu-login_wrapper_form_form_label'} />,
@@ -13,7 +42,30 @@ export const useLogin:UseLogin = () => {
         }
     },[])
 
+    /* 点击提交 */
+    const onSubmit:OnSubmit = useCallback(() => {
+        formInstance.validateFields().then((res:any) => {
+            const values = formInstance.getFieldsValue();
+            //todo 提交数据
+            console.log('获取的数据',values)
+        }).catch((res:any) => {
+            return ;
+        })
+    },[formInstance])
+
+    /* 获取验证码 */
+    const getCode:GetCode = useCallback(() => {
+        formInstance.validateFields(['phone']).then((res:any) => {
+            console.log('通过校验获取校验码')
+        }).catch((res:any) => {
+            console.log(res)
+            return;
+        })
+    },[formInstance])
     return {
         icons,
+        rules,
+        onSubmit,
+        getCode
     }
 }
